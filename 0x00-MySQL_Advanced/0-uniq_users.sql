e4f7395e23928340c442bda94374b7bbc71edb79
TKaburu <traceymwendwa@gmail.com>
2024-07-25 13:08:42 +0000

    feat: We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Create table user that ha id, email and name attributes
+
+CREATE TABLE IF NOT EXIST users (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255) NOT NULL,
+);
